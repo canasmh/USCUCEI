50d9c8d84c682ccf0239bc68b3bdf35a9c167fc4
mannylinesofcode <canasmh@yahoo.com>
2022-02-15 21:40:49 -0500

    Add methods to go to next month and to get data from the calendar

--- greenvillechamber.py
+++ greenvillechamber.py
@@ -1,4 +1,5 @@
 from driver import Driver
+from selenium.webdriver.common.by import By
 
 
 class GreenvilleChamber(Driver):
@@ -6,5 +7,17 @@ class GreenvilleChamber(Driver):
     def __init__(self):
         super().__init__()
         self.url = "https://www.greenvillechamber.org/index.php?src=events&srctype=glance&submenu=_newsevents"
-        self.driver.get(self.url)
         self.events = []
+
+    def go_to_next_month(self):
+        next_button = self.driver.find_element(By.CSS_SELECTOR, "td.nextLink")
+        next_button.click()
+
+    def get_calendar_data(self):
+        calendar = self.driver.find_elements(By.CSS_SELECTOR, "td.eventOn")
+        return calendar
+
+    def get_events(self):
+        self.driver.get(self.url)
+
+        self.driver.quit()
