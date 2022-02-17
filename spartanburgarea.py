01676746988bc7895cb361ae224af2c4154d8ae7
mannylinesofcode <canasmh@yahoo.com>
2022-02-17 00:47:25 -0500

    Get the event cards from Spartanburg Area

--- spartanburgarea.py
+++ spartanburgarea.py
@@ -1,4 +1,5 @@
 from driver import Driver
+from selenium.webdriver.common.by import By
 
 
 class SpartanburgArea(Driver):
@@ -6,3 +7,14 @@ class SpartanburgArea(Driver):
     def __init__(self):
         super().__init__()
         self.url = "http://spartanburgareasc.chambermaster.com/events/"
+
+    def get_events(self):
+        self.driver.get(self.url)
+
+        events = self.driver.find_elements(By.CLASS_NAME, "gz-events-card")
+
+        for event in events:
+            print(event.text)
+
+sa = SpartanburgArea()
+sa.get_events()
