36ca6b21ad49437777d6e5422f96ae1ea55db8a6
mannylinesofcode <canasmh@yahoo.com>
2022-02-17 20:21:39 -0500

    Get date and time of event

--- spartanburgarea.py
+++ spartanburgarea.py
@@ -10,6 +10,37 @@ class SpartanburgArea(Driver):
         super().__init__()
         self.url = "http://spartanburgareasc.chambermaster.com/events/"
 
+    @staticmethod
+    def format_date(date):
+
+        month_conversion = {
+        "Jan": "January",
+        "Feb": "February",
+        "Mar": "March",
+        "Apr": "April",
+        "May": "May",
+        "Jun": "June",
+        "Jul": "July",
+        "Aug": "August",
+        "Sep": "September",
+        "Oct": "October",
+        "Nov": "November",
+        "Dec": "December"
+        }
+
+        new_date = date.split(" ")
+
+        new_date[0]= month_conversion[new_date[0]]
+
+        return " ".join(new_date)
+
+    @staticmethod
+    def format_time(time):
+
+        new_time = time.split(" ")[:-1]
+
+        return " ".join(new_time)
+
     def get_events(self):
 
         self.driver.get(self.url)
@@ -20,6 +51,14 @@ class SpartanburgArea(Driver):
             # Go to event page.
             event = self.driver.find_elements(By.CSS_SELECTOR, ".card-header a")[n]
             self.driver.get(event.get_attribute("href"))
+
+            # Date and time of event
+            event_dt = self.driver.find_elements(By.CSS_SELECTOR, "div.gz-event-date span")
+            # format Weekday Month 01, YYYY
+            date = self.format_date(' '.join(event_dt[0].text.split(" ")[1:]))
+            time = self.format_time(event_dt[1].text)
+
+
 
             # Go back to calendar page.
             back_button = self.driver.find_element(By.CSS_SELECTOR, "div.gz-page-return a")
