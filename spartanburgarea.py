830d1bbdc4fe0216a9b931e5470a3c0b343bc262
mannylinesofcode <canasmh@yahoo.com>
2022-02-17 19:30:56 -0500

    Get webdriver to go to the event page, and then back to the main calendar page

--- spartanburgarea.py
+++ spartanburgarea.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 # TODO: Use href and driver.get instead of click method
 
+
 class SpartanburgArea(Driver):
 
     def __init__(self):
@@ -10,12 +11,19 @@ class SpartanburgArea(Driver):
         self.url = "http://spartanburgareasc.chambermaster.com/events/"
 
     def get_events(self):
+
         self.driver.get(self.url)
 
-        events = self.driver.find_elements(By.CSS_SELECTOR, "a span.gz-img-placeholder")
+        n_events = len(self.driver.find_elements(By.CSS_SELECTOR, ".card-header a"))
 
-        for event in events:
-            print(event.click())
+        for n in range(0, n_events):
+            # Go to event page.
+            event = self.driver.find_elements(By.CSS_SELECTOR, ".card-header a")[n]
+            self.driver.get(event.get_attribute("href"))
+
+            # Go back to calendar page.
+            back_button = self.driver.find_element(By.CSS_SELECTOR, "div.gz-page-return a")
+            self.driver.get(back_button.get_attribute("href"))
         self.driver.quit()
 
 
