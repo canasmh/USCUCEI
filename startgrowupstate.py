0b712599154658afd31d34b43e815cb96b56ecda
mannylinesofcode <canasmh@yahoo.com>
2022-02-21 14:54:13 -0500

    Gather events

--- startgrowupstate.py
+++ startgrowupstate.py
@@ -1,4 +1,5 @@
 from driver import Driver
+from selenium.webdriver.common.by import By
 
 
 class StartGrowUpstate(Driver):
@@ -10,3 +11,6 @@ class StartGrowUpstate(Driver):
 
     def get_events(self):
         self.driver.get(self.url)
+        calendar_object = self.driver.find_element(By.TAG_NAME, "iframe")
+        self.driver.switch_to.frame(calendar_object)
+        events = self.driver.find_elements(By.CSS_SELECTOR, ".calendarRecord")
