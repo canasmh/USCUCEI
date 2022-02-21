f7184b1b1f255f02081a17661b76db2fb07e1ce7
mannylinesofcode <canasmh@yahoo.com>
2022-02-21 17:37:01 -0500

    Scrape details of events

--- startgrowupstate.py
+++ startgrowupstate.py
@@ -1,5 +1,6 @@
 from driver import Driver
 from selenium.webdriver.common.by import By
+import time
 
 
 class StartGrowUpstate(Driver):
@@ -11,6 +12,51 @@ class StartGrowUpstate(Driver):
 
     def get_events(self):
         self.driver.get(self.url)
+        self.driver.maximize_window()
         calendar_object = self.driver.find_element(By.TAG_NAME, "iframe")
         self.driver.switch_to.frame(calendar_object)
+        n = 0
         events = self.driver.find_elements(By.CSS_SELECTOR, ".calendarRecord")
+        n_events = len(events)
+        print(n_events)
+
+        events[n].click()
+        while n < n_events:
+            next_event_button = self.driver.find_element(
+                By.XPATH,
+                '//*[@id="hyperbaseContainer"]/div[15]/div/div/div/div/div[1]/div/div/div/div[1]/div[3]'
+             )
+            next_event_button.click()
+
+            title = self.driver.find_element(
+                By.XPATH,
+                '//*[@id="hyperbaseContainer"]/div[15]/div/div/div/div/div[2]/div/div[2]/div/div[1]/div[1]/div[2]/div/div/div'
+            )
+
+            date = self.driver.find_element(
+                By.XPATH,
+                '//*[@id="hyperbaseContainer"]/div[15]/div/div/div/div/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[1]/div'
+            )
+
+            description = self.driver.find_element(
+                By.XPATH,
+                '//*[@id="hyperbaseContainer"]/div[15]/div/div/div/div/div[2]/div/div[2]/div/div[1]/div[7]/div[2]/div/div/div/div/div[1]'
+            )
+
+            start_time = self.driver.find_element(
+                By.XPATH,
+                '//*[@id="hyperbaseContainer"]/div[15]/div/div/div/div/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div/div/div/div/div[2]/div'
+            )
+
+            link_button = self.driver.find_element(
+                By.XPATH,
+                '//*[@id="hyperbaseContainer"]/div[15]/div/div/div/div/div[2]/div/div[2]/div/div[1]/div[5]/div[2]/div/div/div/a'
+            )
+
+            link = link_button.get_attribute("href")
+
+            print("n: " + str(n) + "\n" + title.text + "\n" + date.text + "\n" + start_time.text + "\n" + end_time.text + "\n" + link + "\n" + description.text + "\n")
+            time.sleep(0.5)
+            n += 1
+
+        self.driver.quit()
