9b567f17d24550bd8cc9a77192112cbdc579c355
mannylinesofcode <canasmh@yahoo.com>
2022-02-27 15:32:46 -0500

    Update working time

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from startgrowupstate import StartGrowUpstate
 from eventsdb import EventsDB
 import time
 
-"Total Time Worked: 21hr10min"
+"Total Time Worked: 21hr25min"
 
 # TODO: Add event data to wordpress
 
