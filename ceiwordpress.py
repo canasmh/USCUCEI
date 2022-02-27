f639f3c5612d75ff589a8554f6f11f4ec1e18103
mannylinesofcode <canasmh@yahoo.com>
2022-02-27 15:34:30 -0500

    Load environment variables. Add url attribute and post_events method -- wip

--- ceiwordpress.py
+++ ceiwordpress.py
@@ -1,11 +1,21 @@
 from driver import Driver
+from selenium.webdriver.common.by import By
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class CEIWordPress(Driver):
 
     def __init__(self):
         super().__init__()
+        self.url = os.environ.get("WP_URL")
+
+    def post_events(self):
+        self.driver.get(self.url)
 
 
 if __name__ == "__main__":
     wp = CEIWordPress()
+    wp.post_events()
