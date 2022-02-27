a783b7141f6173263519781b081239d542c91869
mannylinesofcode <canasmh@yahoo.com>
2022-02-27 15:47:12 -0500

    Add login method. Get the webdriver to the 'add event' page for M.E. Calendar on wordpress

--- ceiwordpress.py
+++ ceiwordpress.py
@@ -2,6 +2,7 @@ from driver import Driver
 from selenium.webdriver.common.by import By
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
@@ -12,8 +13,24 @@ class CEIWordPress(Driver):
         super().__init__()
         self.url = os.environ.get("WP_URL")
 
+    def login(self):
+        username_field = self.driver.find_element(By.ID, "user_login")
+        username_field.send_keys(os.environ.get("WP_USERNAME"))
+        password_field = self.driver.find_element(By.ID, "user_pass")
+        password_field.send_keys(os.environ.get("WP_PASSWORD"))
+        self.driver.find_element(By.ID, "wp-submit").click()
+
     def post_events(self):
         self.driver.get(self.url)
+        self.login()
+        time.sleep(2.5)
+        ME_Calendar = self.driver.find_element(By.XPATH, '//*[@id="toplevel_page_mec-intro"]/a/div[3]')
+        ME_Calendar.click()
+        time.sleep(2.5)
+        add_event_menu_button = self.driver.find_element(By.XPATH, '//*[@id="toplevel_page_mec-intro"]/ul/li[4]/a')
+        add_event_menu_button.click()
+        time.sleep(2.5)
+        self.driver.quit()
 
 
 if __name__ == "__main__":
