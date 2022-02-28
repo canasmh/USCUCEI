6542bd5c3b888fd185bec57d6f00e237825803ad
mannylinesofcode <canasmh@yahoo.com>
2022-02-27 19:46:57 -0500

    Separate some of the actions in post_events method to several different methods

--- ceiwordpress.py
+++ ceiwordpress.py
@@ -6,6 +6,11 @@ import sqlite3
 import os
 import time
 
+
+# TODO: SELECT END TIME -- IF ANY
+# TODO: CHANGE THE POSTED ELEMENT IN DB TO FALSE (ONCE UPLOADED)
+# TODO: ADD EVENT LINK
+
 load_dotenv()
 
 
@@ -35,6 +40,66 @@ class CEIWordPress(Driver):
         add_event_menu_button.click()
         time.sleep(2)
 
+    def add_title(self, title):
+        title_input = self.driver.find_element(By.XPATH, '//*[@id="title"]')
+        title_input.send_keys(title.capitalize())
+
+    def add_description(self, description):
+        self.driver.switch_to.frame(self.driver.find_element(By.XPATH, '//*[@id="content_ifr"]'))
+        text_area = self.driver.find_element(By.XPATH, '//*[@id="tinymce"]')
+        text_area.click()
+        text_area.send_keys(description)
+        self.driver.switch_to.parent_frame()
+
+    def add_date(self, date):
+        start_date_button = self.driver.find_element(By.XPATH, '//*[@id="mec_start_date"]')
+        start_date_button.click()
+        start_month_picker = self.driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/div/div/select[1]')
+        start_month_picker.click()
+        start_month_options = self.driver.find_elements(By.CSS_SELECTOR, "select.ui-datepicker-month option")
+
+        for month in start_month_options:
+            if month.text == date[0:3]:
+                month.click()
+                time.sleep(1.5)
+                break
+        day_options = self.driver.find_elements(By.CSS_SELECTOR, "td[data-handler='selectDay']")
+
+        for day in day_options:
+            event_day = date.split(' ')[1]
+            if day.text == event_day[0:len(event_day) - 1]:
+                day.click()
+
+    def add_start_time(self, event_time):
+        start_time_hr_options = self.driver.find_elements(By.CSS_SELECTOR, "#mec_start_hour option")
+
+        for start_time in start_time_hr_options:
+            start, end = event_time.split(' - ')
+
+            if start_time.text == start.split(':')[0]:
+                start_time.click()
+                break
+
+        start_time_min_options = self.driver.find_elements(By.CSS_SELECTOR, "#mec_start_minutes option")
+
+        for start_time in start_time_min_options:
+            start, end = event_time.split(' - ')
+            min = start.split(':')[1]
+
+            if start_time.text == min[0:2]:
+                start_time.click()
+                break
+
+        start_time_ampm_options = self.driver.find_elements(By.CSS_SELECTOR, "#mec_start_ampm option")
+
+        for ampm in start_time_ampm_options:
+            start, end = event_time.split(' - ')
+            min = start.split(':')[1]
+
+            if ampm.text == min[len(min) - 2: len(min)]:
+                ampm.click()
+                break
+
     def post_events(self):
         self.driver.get(self.url)
         self.login()
@@ -47,70 +112,16 @@ class CEIWordPress(Driver):
             event_time = event[2]
             link = event[3]
             description = event[4]
-            posted = event[5]
+            posted = bool(event[5])
 
-            title_input = self.driver.find_element(By.XPATH, '//*[@id="title"]')
-            title_input.send_keys(title.capitalize())
+            if not posted:
+                self.add_title(title)
+                self.add_description(description)
+                self.add_date(date)
+                self.add_start_time(event_time)
 
-            for i in range(len(title)):
-                title_input.send_keys(Keys.BACKSPACE)
-
-            # Enter description
-            self.driver.switch_to.frame(self.driver.find_element(By.XPATH, '//*[@id="content_ifr"]'))
-            text_area = self.driver.find_element(By.XPATH, '//*[@id="tinymce"]')
-            text_area.click()
-            text_area.send_keys(description)
-            time.sleep(0.5)
-            for i in range(len(description)):
-                text_area.send_keys(Keys.BACKSPACE)
-            self.driver.switch_to.parent_frame()
-            start_date_button = self.driver.find_element(By.XPATH, '//*[@id="mec_start_date"]')
-            start_date_button.click()
-            start_month_picker = self.driver.find_element(By.XPATH, '//*[@id="ui-datepicker-div"]/div/div/select[1]')
-            start_month_picker.click()
-            start_month_options = self.driver.find_elements(By.CSS_SELECTOR, "select.ui-datepicker-month option")
-
-            for month in start_month_options:
-                if month.text == date[0:3]:
-                    month.click()
-                    time.sleep(1.5)
-                    break
-            day_options = self.driver.find_elements(By.CSS_SELECTOR, "td[data-handler='selectDay']")
-
-            for day in day_options:
-                event_day = date.split(' ')[1]
-                if day.text == event_day[0:len(event_day) - 1]:
-                    day.click()
-
-            start_time_hr_options = self.driver.find_elements(By.CSS_SELECTOR, "#mec_start_hour option")
-
-            for start_time in start_time_hr_options:
-                start, end = event_time.split(' - ')
-
-                if start_time.text == start.split(':')[0]:
-                    start_time.click()
-                    break
-
-            start_time_min_options = self.driver.find_elements(By.CSS_SELECTOR, "#mec_start_minutes option")
-
-            for start_time in start_time_min_options:
-                start, end = event_time.split(' - ')
-                min = start.split(':')[1]
-
-                if start_time.text == min[0:2]:
-                    start_time.click()
-                    break
-
-            start_time_ampm_options = self.driver.find_elements(By.CSS_SELECTOR, "#mec_start_ampm option")
-
-            for ampm in start_time_ampm_options:
-                start, end = event_time.split(' - ')
-                min = start.split(':')[1]
-
-                if ampm.text == min[len(min) - 2: len(min)]:
-                    ampm.click()
-                    break
-
+            else:
+                continue
 
             time.sleep(0.5)
             print(event)
