614c3793aca4b9cc14b906c190603427af4ee4c3
mannylinesofcode <canasmh@yahoo.com>
2022-03-23 22:29:51 -0400

    Install Chrome driver real time

--- driver.py
+++ driver.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import os
 
 
 class Driver:
 
     def __init__(self):
-        self.driver_path = os.getcwd() + "/chromedriver"
-        self.driver = webdriver.Chrome(self.driver_path)
+        self.service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(service=self.service)
 
         
