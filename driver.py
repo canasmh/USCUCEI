258107d07ce2c5a5e26cb5be15bc7022d0fa383e
canasmh <canasmh@yahoo.com>
2022-04-15 21:40:56 -0400

    Remove the chrome GUI

--- driver.py
+++ driver.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-import os
 
 
 class Driver:
@@ -9,6 +8,8 @@ class Driver:
 
     def __init__(self):
         self.service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=self.service)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        self.driver = webdriver.Chrome(service=self.service, options=chrome_options)
 
         
